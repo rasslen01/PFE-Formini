3adacf96cd2c3d112509aac67e1e91e33b60e9e4
Rasslen Benassi <160419199+rasslen01@users.noreply.github.com>
2026-02-19 16:06:22 +0300

    Update GestionDesFormations.js

--- src/views/admin/GestionDesFormations.js
+++ src/views/admin/GestionDesFormations.js
@@ -2,14 +2,14 @@ import React from "react";
 
 // components
 
-import CardTable from "components/Cards/CardTableFormations";
+import CardTableFormations from "components/Cards/CardTableFormations";
 
 export default function Tables() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
-        <div className="w-full mb-12 px-4">
-          <CardTable />
+        <div className="w-full mb-12 px-4 mt-10">
+          <CardTableFormations />
         </div>
         {/* <div className="w-full mb-12 px-4">
           <CardTable color="dark" />
